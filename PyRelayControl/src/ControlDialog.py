24fda169b9b1e2121256b3084145b0820b57a44e
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-05-23 07:39:26 +0200

    Update ControlDialog.py

--- PyRelayControl/src/ControlDialog.py
+++ PyRelayControl/src/ControlDialog.py
@@ -36,7 +36,6 @@ class ControlDialog(AppletDialog):
 
         # members required by _buildUi() must be set before calling super().__init__()
         self._propSettings = prop_settings
-        self._groupBoxes = {}
 
         if 'prop' in self._propSettings and 'json' in self._propSettings['prop']:
             self._propVariables = PropPanel.getJson(self._propSettings['prop']['json'], logger)
@@ -60,18 +59,20 @@ class ControlDialog(AppletDialog):
     @pyqtSlot()
     def _buildPropWidgets(self):
 
-        for group in list(self._groupBoxes.keys()):
-            widgets = self._groupBoxes[group].findChildren(PinSwitch, '', options=Qt.FindChildrenRecursively)
+        group_boxes = {}
+
+        for group in list(group_boxes.keys()):
+            widgets = group_boxes[group].findChildren(PinSwitch, '', options=Qt.FindChildrenRecursively)
             for w in widgets:
                 try:
-                    self._groupBoxes[group].layout().removeWidget(w)
+                    group_boxes[group].layout().removeWidget(w)
                     w.deleteLater()
                 except Exception as e:
                     print(e)
             del (widgets)
-            self._mainLayout.removeWidget(self._groupBoxes[group])
-            self._groupBoxes[group].deleteLater()
-            del (self._groupBoxes[group])
+            self._mainLayout.removeWidget(group_boxes[group])
+            group_boxes[group].deleteLater()
+            del (group_boxes[group])
 
         for v, pin in self._propVariables.items():
             if '/' in v:
@@ -91,29 +92,31 @@ class ControlDialog(AppletDialog):
                                value_on=pin.getHigh(),
                                value_off=pin.getLow(),
                                topic=self._propSettings['prop']['prop_inbox'])
-            if group in self._groupBoxes:
-                self._groupBoxes[group].layout().addWidget(switch)
+            if group in group_boxes:
+                group_boxes[group].layout().addWidget(switch)
             else:
                 caption = group.capitalize() if group is not None else ''
                 box = QGroupBox(caption)
                 box_layout = QVBoxLayout(box)
                 box_layout.setSpacing(12)
-                self._groupBoxes[group] = box
+                group_boxes[group] = box
                 self._mainLayout.addWidget(box)
                 box_layout.addWidget(switch)
             switch.publishMessage.connect(self.publishMessage)
             self.propDataReveived.connect(switch.onDataReceived)
 
-        for group in list(self._groupBoxes.keys()):
+        for group in list(group_boxes.keys()):
             if group is None: continue
             button_on = PinGroupButton(group, GPIO_HIGH, self._propSettings['prop']['prop_inbox'])
-            self._groupBoxes[group].layout().addWidget(button_on)
+            group_boxes[group].layout().addWidget(button_on)
 
             button_off = PinGroupButton(group, GPIO_LOW, self._propSettings['prop']['prop_inbox'])
-            self._groupBoxes[group].layout().addWidget(button_off)
+            group_boxes[group].layout().addWidget(button_off)
 
             button_on.publishMessage.connect(self.publishMessage)
             button_off.publishMessage.connect(self.publishMessage)
+
+        self.publishMessage.emit(self._propSettings['prop']['prop_inbox'], 'app:data')
 
     # __________________________________________________________________
     def _buildUi(self):
@@ -221,6 +224,8 @@ class ControlDialog(AppletDialog):
                                                                        self.tr("connected")))
             else:
                 self._led.switchOn('yellow')
+
+        self.publishMessage.emit(self._propSettings['prop']['prop_inbox'], 'app:data')
 
     # __________________________________________________________________
     @pyqtSlot()
