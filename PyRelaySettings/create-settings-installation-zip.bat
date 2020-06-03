20006573399aa85256861826c9e681a1c240472f
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-06-03 18:41:17 +0200

    Remove PyRelaySettings

--- PyRelaySettings/create-settings-installation-zip.bat
+++
@@ -1,1 +0,0 @@
-C:\"Program Files"\7-Zip\7z.exe a -tzip RelaySettingsInstallation.zip -xr!.git -xr!ready -xr!_SYNCAPP -xr!*.tgz -xr!*.vs -xr!*.pyc -xr!*.log.* -xr!.eric* -xr!_eric* -xr!__* -xr!venv -xr!.config.yml -xr!.layout.yml -xr!create-*.bat ./ ../core
